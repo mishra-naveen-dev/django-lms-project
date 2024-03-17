2c79611738d5eb31a447f565c005530f6bdd506a
nvineng <naveen211063cse@sdbc.ac.in>
2024-03-18 00:04:45 +0530

    Add watch course and video links

--- LMS/user_login.py
+++ LMS/user_login.py
@@ -14,7 +14,7 @@ def REGISTER(request):
        username = request.POST.get('username')
        email = request.POST.get('email')
        password = request.POST.get('password')
-       print(username,email,password)
+    #    print(username,email,password)
        #    check username
        if User.objects.filter(email=email).exists():
            messages.warning(request,'Email are Already Exists !')
